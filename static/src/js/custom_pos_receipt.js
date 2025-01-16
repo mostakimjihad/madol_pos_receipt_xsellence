e684f285de1524d732b08dbf443b28a41aadf1c2
mostakimjihad <mostakimjihad652001@gmail.com>
2025-01-16 18:42:59 +0600

    orderline price without VAT

--- static/src/js/custom_pos_receipt.js
+++ static/src/js/custom_pos_receipt.js
@@ -4,30 +4,18 @@ import { registry } from "@web/core/registry";
 import { patch } from "@web/core/utils/patch";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 
-//setup() {
-//    this.rpc = useService("rpc");
-//};
-
-//const result = await this.rpc("/hr_attendance/attendance_user_data");
-
-//return this.rpc({
-//    model: 'some.model',
-//    method: 'some_method',
-//    args: [some, args],
-//});
 
 patch(PosStore.prototype, {
    getReceiptHeaderData() {
 
-       console.log('------------');
-       console.log(this);
-       console.log('------------');
+    //    console.log('------------');
+    //    console.log(this);
+    //    console.log('------------');
 
        var ref = ""
 
        for (const orderline of this.selectedOrder.orderlines) {
             if (orderline.product.taxes_id && orderline.product.taxes_id.length > 0) {
-                console.log(orderline.product.taxes_id);
                 ref += orderline.full_product_name + ",";
             }
         }
@@ -36,6 +24,11 @@ patch(PosStore.prototype, {
            ...super.getReceiptHeaderData(...arguments),
            partner: this.get_order().get_partner(),
            ref: ref,
+           orderlines_price_and_name: this.selectedOrder.orderlines.map(orderline => ({
+            price: orderline.price,
+            product_name: orderline.full_product_name,
+            quantity: orderline.quantity
+        })),
        };
    },
 });
